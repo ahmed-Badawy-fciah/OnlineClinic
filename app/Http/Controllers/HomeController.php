7912f2b81df59fae6f2b2e170f8bd5265659d64a
ahmed-Badawy-fciah <ahmedbadawy.fcia@gmail.com>
2020-05-19 05:45:55 +0200

    before Start In My Task

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests;
+use Illuminate\Http\Request;
+use Auth;
+class HomeController extends Controller
+{
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
+    /**
+     * Show the application dashboard.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return view('home');
+    }
+
+public function control()
+{
+    if(Auth::user()->role <1){
+        return redirect('/');
+    }
+
+    return view('control');
+}
+public function booking()
+{
+    if(Auth::user()->role >3){
+        return redirect('/');
+    }
+
+    return view('booking');
+}
+public function appointments()
+{
+    if(Auth::user()->role =!2){
+        return redirect('/');
+    }
+
+    return view('appointments');
+}
+public function about()
+    {
+        return view('about');
+    }
+    public function connect_us()
+    {
+        return view('connect_us');
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -2,15 +2,24 @@
 
 /*
 |--------------------------------------------------------------------------
-| Web Routes
+| Application Routes
 |--------------------------------------------------------------------------
 |
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
+| Here is where you can register all of the routes for an application.
+| It's a breeze. Simply tell Laravel the URIs it should respond to
+| and give it the controller to call when that URI is requested.
 |
 */
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::auth();
+
+Route::get('/home', 'HomeController@index');
+Route::get('/control', 'HomeController@control');
+Route::get('/booking', 'HomeController@booking');
+Route::get('/appointments', 'HomeController@appointments');
+Route::get('/about', 'HomeController@about');
+Route::get('/connect_us', 'HomeController@connect_us');
